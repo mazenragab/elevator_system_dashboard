8124e421e025f36aa552d0d14c12143a426c266d
Mazen <109856999+mazenragab@users.noreply.github.com>
2025-12-03 22:48:01 +0200

    api client

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-const url_global = "https://elevator-system-mu.vercel.app/"
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://elevator-system-mu.vercel.app/api/v1';
+const url_global = "https://elevator-system-mu.vercel.app/api/v1"
+const local_url = "http://localhost:3000/api/v1"
+const API_BASE_URL = local_url
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
